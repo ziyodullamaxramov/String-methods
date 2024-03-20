535962e8495f2bc1c5750b4740b60bc6f568e601
Ziyodulla <ziyodullamaxramov@gmail.com>
2024-03-20 14:23:22 +0500

    Added if/else

--- app.js
+++ app.js
@@ -5,25 +5,31 @@ var array = [];
 
 
 function uUnshift() {
-    array.unshift(userEnter.value);
-    res.innerHTML = array
-    userEnter.value = '';
-}
+    if (userEnter.value != '') {
+        array.unshift(userEnter.value);
+        res.innerHTML = array
+        userEnter.value = '';
+    }
+};
 function uPush() {
-    array.push(userEnter.value);
-    res.innerHTML = array
-    userEnter.value = '';
+    if (userEnter.value != '') {
+        array.push(userEnter.value);
+        res.innerHTML = array
+        userEnter.value = '';
+    }
 }
 function uShift() {
     array.shift(userEnter.value);
     res.innerHTML = array
-}
+};
 function uPop() {
     array.pop(userEnter.value);
     res.innerHTML = array
-}
+};
 function uJoin() {
-    var joinedArr = array.join(usJoin.value);
-    res.innerHTML = joinedArr
-    usJoin.value = '';
-}
+    if (usJoin.value != '') {
+        var joinedArr = array.join(usJoin.value);
+        res.innerHTML = joinedArr
+        usJoin.value = '';
+    }
+};
